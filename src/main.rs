8bdd31a10592a5ddfaa66309637321450c6b03da
Louis Dutton <louis.dutton@travelchapter.com>
2024-07-10 19:40:57 +0000

    read example file

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ mod actions;
 mod editor;
 mod render;
 
+use std::{fs, path::Path};
+
 use editor::Editor;
 
 fn main() {
-    let mut editor = Editor::new().unwrap();
-    _ = editor.start();
+    let text = fs::read_to_string(Path::new("./example.txt")).expect("Failed to read file");
+    let mut editor = Editor::new().expect("Failed to initialise editor");
+    editor.text = text;
+    editor.start().expect("Failed to start editor event loop")
 }
