fdbcc6de74cf36ac19ece2472562f7ecc2cdce95
Louis Dutton <louis.dutton@travelchapter.com>
2024-07-13 15:54:30 +0000

    read file from args using clap

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,31 @@ mod actions;
 mod editor;
 mod render;
 
+use clap::{command, Parser};
 use std::{fs, io, path::Path};
 
 use editor::Editor;
 
+#[derive(Parser)]
+#[command(name = "korsakov")]
+#[command(bin_name = "kors")]
+#[command(version, about, long_about = None)]
+/// Korsakov: A speedy little text editor for the terminal.
+struct Args {
+    /// The target file path
+    file: String,
+}
+
 fn main() -> io::Result<()> {
-    _ = Editor::new()?
-        .set_text(fs::read_to_string(Path::new("./example.txt"))?)
-        .listen()?;
+    let args = Args::parse();
+    let mut editor = Editor::new()?;
+
+    if !args.file.is_empty() {
+        if let Ok(text) = fs::read_to_string(Path::new(&args.file)) {
+            editor.set_text(text);
+        }
+    }
+
+    editor.listen()?;
     Ok(())
 }
