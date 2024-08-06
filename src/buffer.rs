c47aa633a87f948350a3d9f25a24c461f63ca8bc
Louis Dutton <louis.dutton@travelchapter.com>
2024-08-06 15:23:45 +0000

    feat: apply selection to given buffer or string

--- src/buffer.rs
+++ src/buffer.rs
@@ -12,6 +12,13 @@ pub struct Buffer {
     pub selection: Option<Selection>,
 }
 
+impl Selection {
+    /// Returns a substring of the provided content based on the current bounds of the selection.
+    pub fn apply<'a>(&self, content: &'a str) -> Option<&'a str> {
+        content.get((self.start as usize)..(self.end as usize))
+    }
+}
+
 impl Buffer {
     pub fn new(name: String, content: String) -> Self {
         Buffer {
