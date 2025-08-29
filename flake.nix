ae24af98c2f26f3724be6cb525bc6101d66aad1a
Louis Dutton <louis@dutton.digital>
2025-08-29 21:22:54 +0100

    ols config

--- flake.nix
+++ flake.nix
@@ -31,27 +31,16 @@
               odin
             ];
 
-            packages = [
-              # terminal/ui libraries that might be needed
-              ncurses
-
-              # tree-sitter for syntax highlighting
+            buildInputs = [
               tree-sitter
+            ];
 
-              # debugging
+            packages = [
               gdb
-              lldb
-
-              # language support
               ols
               nixd
               alejandra
             ];
-
-            shellHook = ''
-              echo "Korsakov Odin development environment ready!"
-              echo "Odin version: $(odin version)"
-            '';
           };
         }
     );
